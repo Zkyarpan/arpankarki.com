237dd21923a63705e63069c2694d164b7ab02af2
Zkyarpan <karkiarpan555@gmail.com>
2024-08-21 16:13:39 +0545

    update globals.css and Projects.tsx

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,6 +3,7 @@ import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Image from "next/image";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 
 const portfolioProjects = [
   {
@@ -45,58 +46,68 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <div className="container">
-      <div className="flex justify-center">
-        <p className="uppercase font-semibold tracking-widest text-center text-transparent bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text w-full">
-          Real-World Results
+    <section className="pb-16 lg:py-24">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest text-center text-transparent bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text w-full">
+            Real-World Results
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+          provident voluptatibus quis doloribus consequatur aliquid.
         </p>
-      </div>
-      <h2 className="font-serif text-3xl text-center mt-6">
-        Featured Projects
-      </h2>
-      <p className="text-center text-white/60 mt-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit provident
-        voluptatibus quis doloribus consequatur aliquid.
-      </p>
-      <div className="flex flex-col mt-10 space-y-10">
-        {portfolioProjects.map((project) => (
-          <div
-            key={project.title}
-            className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 p-8"
-          >
-            <div className="flex">
-              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-                {project.company}
-                <span>&bull;</span>
-                <span className="">{project.year}</span>
+        <div className="flex md:mt-20 flex-col mt-10 space-y-10">
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none"
+            >
+              <div className="lg:grid lg:grid-cols-2">
+                <div>
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                    {project.company}
+                    <span>&bull;</span>
+                    <span className="">{project.year}</span>
+                  </div>
+                  <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result) => (
+                      <li
+                        className="flex gap-2 text-sm md:text-base text-white/50"
+                        key={result.title}
+                      >
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href={project.link}>
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                      <span>Visit Live Site</span>
+                      <ArrowUpRightIcon className="size-4" />
+                    </button>
+                  </a>
+                </div>
+<div>
+  
+</div>
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  className="mt-8 -mb-4 md:-mb-0"
+                />
               </div>
             </div>
-            <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
-            <hr className="border-t-2 border-white/5 mt-4" />
-            <ul className="flex flex-col gap-4 mt-4">
-              {project.results.map((result) => (
-                <li
-                  className="flex gap-2 text-sm text-white/50"
-                  key={result.title}
-                >
-                  <CheckCircleIcon className="size-5" />
-                  <span>{result.title}</span>
-                </li>
-              ))}
-            </ul>
-            <a href={project.link}>
-              <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold">
-                View Live Site
-              </button>
-            </a>
-            <Image
-              src={project.image}
-              alt={project.title}
-              className="mt-6 rounded-lg"
-            />
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
