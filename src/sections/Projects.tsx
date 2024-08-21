7a6f02b79645164f139e4f3b83ed3e52ac5d0a0e
Zkyarpan <karkiarpan555@gmail.com>
2024-08-21 11:23:05 +0545

    update Projects.tsx

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,7 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import Image from "next/image";
 
 const portfolioProjects = [
   {
@@ -44,25 +45,47 @@ const portfolioProjects = [
 export const ProjectsSection = () => {
   return (
     <div className="container">
-      <p>Real-World Results</p>
-      <h2>Featured Projects</h2>
-      <p>
+      <div className="flex justify-center">
+        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to sky-400 text-center bg-clip-text text-transparent">
+          Real-World Results
+        </p>
+      </div>
+      <h2 className="font-serif text-3xl text-center mt-6">
+        Featured Projects
+      </h2>
+      <p className="text-center text-white/60 mt-4">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit provident
         voluptatibus quis doloribus consequatur aliquid.
       </p>
-      <div>
+      <div className="flex flex-col mt-10 space-y-10">
         {portfolioProjects.map((project) => (
-          <div key={project.title}>
-            <div>
-              <span>{project.title}</span>
-              <span>{project.year}</span>
+          <div
+            key={project.title}
+            className="bg-gray-800 p-6 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20"
+          >
+            <div className="flex justify-between">
+              <span className="font-semibold text-lg">{project.title}</span>
+              <span className="text-gray-400">{project.year}</span>
             </div>
-            <h3>{project.title}</h3>
-            <ul>
+            <h3 className="text-xl mt-4">
+              {project.title}
+              <span className="text-gray-400">Project Details</span>
+            </h3>
+            <ul className="mt-4 space-y-2">
               {project.results.map((result) => (
-                <li key={project.link}>{result.title}</li>
+                <li key={result.title}>{result.title}</li>
               ))}
             </ul>
+            <a href={project.link}>
+              <button className="mt-6 bg-blue-500 text-white py-2 px-4 rounded">
+                View Live Site
+              </button>
+            </a>=
+            <Image
+              src={project.image}
+              alt={project.title}
+              className="mt-6 rounded-lg"
+            />
           </div>
         ))}
       </div>
