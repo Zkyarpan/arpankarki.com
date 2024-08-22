c38e3cf5b50640a3ddef85a8988e77fc33c7e02c
Zkyarpan <karkiarpan555@gmail.com>
2024-08-22 09:59:14 +0545

    Complete Part One

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,6 +1,38 @@
+import StartIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Responsive",
+  "Scalable",
+  "Versatile",
+  "Performance-centric",
+  "Accessibility-focused",
+  "Security-focused",
+  "Responsive-focused",
+  "Scalable-focused",
+  "Versatile-focused",
+  "Performance-centric-focused",
+];
 
 export const TapeSection = () => {
-  return <div>
-
-  </div>;
+  return (
+    <div className="py-16 overflow-x-clip lg:py-24">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90% transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+            {words.map((word, index) => (
+              <div key={index} className="inline-flex gap-4 items-center">
+                <span className="text-gray-900 font-serif font-extrabold text-sm -mx-1">
+                  {word}
+                </span>
+                <StartIcon className="size-6 text-gray-900 -rotate-12" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
