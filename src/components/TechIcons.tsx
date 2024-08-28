4361f4079df6723c1b47c37ae9e3927a48ecd3e5
Zkyarpan <karkiarpan555@gmail.com>
2024-08-28 21:33:11 +0545

    Adding gradient color to the skill icons

--- src/components/TechIcons.tsx
+++ src/components/TechIcons.tsx
@@ -3,9 +3,15 @@ import React from "react";
 const TechIcons = ({ component }: { component: React.ElementType }) => {
   const Component = component;
   return (
-    <div>
-      <Component className="size-10" />
-    </div>
+    <>
+      <Component className="w-10 h-10 fill-[url(#tech-icon-gradient)]" />
+      <svg className="w-0 h-0 absolute">
+        <linearGradient id="tech-icon-gradient">
+          <stop offset="0%" stopColor="rgb(110, 231, 183)" />
+          <stop offset="100%" stopColor="rgb(56, 189, 248)" />
+        </linearGradient>
+      </svg>
+    </>
   );
 };
 
