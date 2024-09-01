e94aeacd6719c678a3d988b403f4f7e598c75c84
Zkyarpan <karkiarpan555@gmail.com>
2024-09-01 22:32:34 +0545

    build(deps): update package-lock.json, Projects.tsx and Testimonials.tsx

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,49 +1,31 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import socialMediaApp from "@/assets/images/social.png";
+import blogApp from "@/assets/images/blog.png";
 import Image from "next/image";
-import CheckCircleIcon from "@/assets/icons/check-circle.svg";
-import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import SectionHeader from "@/components/SectionHeader";
 import grainImage from "@/assets/images/grain.jpg";
 import Card from "@/components/Card";
+import { FaGithub } from "react-icons/fa";
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
-  },
-  {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
-    results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
-    ],
-    link: "https://youtu.be/7hi5zwO75yc",
-    image: lightSaasLandingPage,
-  },
-  {
-    company: "Quantum Dynamics",
+    company: "Social Connect",
     year: "2023",
-    title: "AI Startup Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
-    image: aiStartupLandingPage,
+    title: "Social Media App",
+    description:
+      "Connect with friends, share activities, and follow others in real-time. Stay updated with the latest posts and engage with content effortlessly.",
+    link: "https://github.com/Zkyarpan/PostMe",
+    image: socialMediaApp,
+    showLink: true,
+  },
+  {
+    company: "ThoughtWave",
+    year: "2022",
+    title: "Blog App",
+    description:
+      "Share your thoughts and ideas. Discover, create, and engage with meaningful content that resonates with your interests.",
+    link: "https://github.com/Zkyarpan/blogVibe",
+    image: blogApp,
+    showLink: true,
   },
 ];
 
@@ -76,35 +58,33 @@ export const ProjectsSection = () => {
                   <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
                     {project.company}
                     <span>&bull;</span>
-                    <span className="">{project.year}</span>
+                    <span>{project.year}</span>
                   </div>
                   <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
                     {project.title}
                   </h3>
-                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
-                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                    {project.results.map((result) => (
-                      <li
-                        className="flex gap-2 text-sm md:text-base text-white/50"
-                        key={result.title}
-                      >
-                        <CheckCircleIcon className="size-5 md:size-6" />
-                        <span>{result.title}</span>
-                      </li>
-                    ))}
-                  </ul>
-                  <a href={project.link}>
-                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-                      <span>Visit Live Site</span>
-                      <ArrowUpRightIcon className="size-4" />
-                    </button>
-                  </a>
+                  <p className="text-sm md:text-base text-white/75 mt-4">
+                    {project.description}
+                  </p>
+                  {project.showLink && (
+                    <a
+                      href={project.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="inline-block"
+                    >
+                      <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                        <FaGithub className="size-4" />
+                        <span>Source Code</span>
+                      </button>
+                    </a>
+                  )}
                 </div>
                 <div>
                   <Image
                     src={project.image}
                     alt={project.title}
-                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none rounded-lg"
                   />
                 </div>
               </div>
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,8 @@
-import memojiAvatar1 from "@/assets/images/memoji-avatar-1.png";
-import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
-import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
-import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
-import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import monday from "@/assets/images/monday.jpg";
+import arpan1 from "@/assets/images/arpan1.jpg";
+import arpan2 from "@/assets/images/arpan2.jpg";
+import rahul from "@/assets/images/rahul.jpg";
+import suman from "@/assets/images/suman.jpg";
 import SectionHeader from "@/components/SectionHeader";
 import Image from "next/image";
 import Card from "@/components/Card";
@@ -13,31 +13,31 @@ const testimonials = [
     name: "Alex Turner",
     position: "Marketing Manager @ TechStartups",
     text: "Arpan, despite being a student, made a significant impact on our website. His attention to detail and understanding of our brand were impressive. We're very pleased with the results!",
-    avatar: memojiAvatar1,
+    avatar: monday,
   },
   {
     name: "Olivia Green",
     position: "Head of Design @ GreenLeaf",
     text: "Working with Arpan was great. His skills as a software engineer brought our designs to life beyond our expectations. The end result was fantastic.",
-    avatar: memojiAvatar2,
+    avatar: arpan1,
   },
   {
     name: "Daniel White",
     position: "CEO @ InnovateCo",
     text: "Arpan's ability to create smooth user experiences is remarkable. We’ve seen a notable increase in conversions since the new design was launched. We’re very satisfied with his work.",
-    avatar: memojiAvatar3,
+    avatar: arpan2,
   },
   {
     name: "Emily Carter",
     position: "Product Manager @ GlobalTech",
     text: "Arpan is a skilled software engineer who turned our complex product into an intuitive interface. We've received great feedback from our users.",
-    avatar: memojiAvatar4,
+    avatar: rahul,
   },
   {
     name: "Michael Brown",
     position: "Director of IT @ MegaCorp",
     text: "Arpan’s work on our website has been outstanding. Even as a student, his skills and communication are excellent. We highly recommend him.",
-    avatar: memojiAvatar5,
+    avatar: suman,
   },
 ];
 
@@ -48,34 +48,42 @@ export const TestimonialsSection = () => {
         <SectionHeader
           eyebrow="Happy Clients"
           title="What clients say about me"
-          description=" Don't just take my word for it. See what my clients have to say
-        about my work."
+          description="Don't just take my word for it. See what my clients have to say about my work."
         />
         <div className="mt-12 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] py-4 -my-4">
-          <div className="flex gap-8 pr-8 flex-none animate-move-left [animation-duration:90s] hover:[animation-play-state:paused]">
+          <div className="flex gap-8 pr-8 flex-none animate-move-left [animation-duration:50s] hover:[animation-play-state:paused]">
             {[...new Array(2)].fill(0).map((_, index) => (
-              <Fragment key={index}></Fragment>
-            ))}
-            {testimonials.map((item) => (
-              <Card
-                key={item.name}
-                className="max-w-xs md:max-w-md md:p-8 hover:-rotate-3 transition duration-300"
-              >
-                <div className="flex gap-4 items-center">
-                  <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
-                    <Image
-                      src={item.avatar}
-                      alt={item.name}
-                      className="max-h-full"
-                    />
-                  </div>
-                  <div>
-                    <div className="font-semibold">{item.name}</div>
-                    <div className="text-sm text-white/40">{item.position}</div>
-                  </div>
-                </div>
-                <p className="mt-4 md:mt-6 text-sm md:text-base">{item.text}</p>
-              </Card>
+              <Fragment key={index}>
+                {testimonials.map((item) => (
+                  <Card
+                    key={item.name}
+                    className="max-w-xs md:max-w-md p-6 md:p-8 bg-gray-800  hover:-rotate-3 transition duration-300"
+                  >
+                    <div className="flex gap-4 items-center">
+                      <div className="w-14 h-14 bg-gray-700 rounded-full overflow-hidden flex-shrink-0">
+                        <Image
+                          src={item.avatar}
+                          alt={item.name}
+                          width={56}
+                          height={56}
+                          className="object-cover w-full h-full"
+                        />
+                      </div>
+                      <div>
+                        <div className="font-semibold text-white">
+                          {item.name}
+                        </div>
+                        <div className="text-sm text-white/60">
+                          {item.position}
+                        </div>
+                      </div>
+                    </div>
+                    <p className="mt-4 md:mt-6 text-sm md:text-base text-white/80">
+                      {item.text}
+                    </p>
+                  </Card>
+                ))}
+              </Fragment>
             ))}
           </div>
         </div>
