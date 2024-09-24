e2b59a3ff77aaba70fc4c8b257f21cca5c1d82aa
Zkyarpan <karkiarpan555@gmail.com>
2024-09-24 22:30:47 +0545

    update text-hover-effect.tsx and TextHover.tsx

--- src/sections/TextHover.tsx
+++ src/sections/TextHover.tsx
@@ -3,7 +3,7 @@ import { TextHoverEffect } from "@/components/ui/text-hover-effect";
 
 export function TextHover() {
   return (
-    <div className="h-[40rem] flex items-center justify-center">
+    <div className="h-[20rem] flex items-center justify-center">
       <TextHoverEffect text="ARPAN" />
     </div>
   );
